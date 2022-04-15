39153e6ff59116963c10e60a278c4c06f524f0a7
EC2 Default User <ec2-user@ebhorse.localdomain>
2022-04-15 20:17:31 +0000

    adding some s3 code

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import os
+import boto3
 
 PORT = 8080
 name = os.environ.get('NAME')
@@ -10,12 +11,25 @@ print("Message: '" + MESSAGE + "'")
 
 app = Flask(__name__)
 
+s3bucket = "cbucket-test-3"
+s3key = "FOOBAR.txt"
 
 @app.route("/")
 def root():
   print("Handling web request. Returning message.")
   result = MESSAGE.encode("utf-8")
   return result
+
+@app.route("/foobar")
+def foobar():
+  print("Handling web request for foobar. Returning contents...")
+  s3 = boto3.client('s3')
+  with open('temps3file', 'wb') as f:
+    s3.download_fileobj(s3bucket, s3key, f)
+    line = f.read(10)
+    print ("Read Line: %s" % (line))
+    result = line.encode("utf-8")
+    return result
 
 
 if __name__ == "__main__":
